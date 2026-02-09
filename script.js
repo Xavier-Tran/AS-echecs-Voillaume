75180ba5bb54cec41f64f4802027e032e7d559c9
Xavier-Tran <littleshot.tran@gmail.com>
2026-02-09 06:32:59 +0100

    Update script.js

--- script.js
+++ script.js
@@ -110,11 +110,11 @@ const matches = [
   { "id": 60, "date": "2025-12-10", "player1": "rayaneZ", "player2": "Weyweyy", "result": "win" },
   { "id": 61, "date": "2025-12-10", "player1": "RSK", "player2": "BlacKHawK5", "result": "win" },
   { "id": 62, "date": "2026-01-11", "player1": "P2N", "player2": "LUCKYBOY", "result": "win" },
-  { "id": 60, "date": "2025-01-19", "player1": "RSK", "player2": "LUCKYBOY", "result": "win" },
-  { "id": 61, "date": "2025-01-19", "player1": "LUCKYBOY", "player2": "P2N", "result": "win" },
-  { "id": 62, "date": "2026-01-11", "player1": "RSK", "player2": "P2N", "result": "win" },
-  { "id": 63, "date": "2025-02-04", "player1": "Shadow", "player2": "SHIFTY", "result": "win" },
-  { "id": 64, "date": "2025-02-04", "player1": "Shadow", "player2": "SHIFTY", "result": "win" },
+  { "id": 63, "date": "2025-01-19", "player1": "RSK", "player2": "LUCKYBOY", "result": "win" },
+  { "id": 64, "date": "2025-01-19", "player1": "LUCKYBOY", "player2": "P2N", "result": "win" },
+  { "id": 65, "date": "2026-01-11", "player1": "RSK", "player2": "P2N", "result": "win" },
+  { "id": 66, "date": "2025-02-04", "player1": "Shadow", "player2": "SHIFTY", "result": "win" },
+  { "id": 67, "date": "2025-02-04", "player1": "Shadow", "player2": "SHIFTY", "result": "win" },
 ];
 
 let joueurs = [];
